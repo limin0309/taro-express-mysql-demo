65403c6b499faaec59576caed4dd1079c1156431
lm <13693576511@163.com>
2023-11-14 13:57:00 +0800

    eslint-配置

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,17 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+
+const a = 2;
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
+    <View className="index">
+      <Text>Hello worldhaha!</Text>
     </View>
-  )
+  );
 }
